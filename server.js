558193abec54e426b708421a928e0626212f1b9f
Lawe Zangena <lawza02@gmail.com>
2021-09-29 11:58:14 +0200

    ja

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const http = require('http');
+
+const fs = require('fs')
+
+const server = http.createServer();
+
+server.on('request',(req,res) => {
+    
+    fs.readFile('index.httml', function (err, data) {
+         
+         res.statusCode = 200;
+         res.setHeader('Content-Length', data.length);
+         res.setHeader('Contetnt type', 'text/html');
+         res.write(data);
+         res.end();
+      });
+
+});
+
+
+const PORT = process.env.PORT || 8042;
+
+ 
+server.listen(PORT);
+
+console.log(`Server up and listening on port ${PORT}`);
+ 
