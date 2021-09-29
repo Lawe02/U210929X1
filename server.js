12d59715c34dc3aac509fa3dcd5cb5bd02245738
Lawe Zangena <lawza02@gmail.com>
2021-09-29 13:07:29 +0200

    update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+'use strict'
+
 const express = require('express')
+const fs = requiere ('fs')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hellooooooo')
 })
 
 app.listen(port, () => {
